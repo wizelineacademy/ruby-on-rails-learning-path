d267f4ecdc19ab9e22f2f06014bb885f6eb4c63c
Victor Smith <victor.smith@wizeline.com>
2021-09-21 20:13:09 -0500

    [FEAT] User model rspec tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context "user data" do
+    it "must contain user_name, email and password" do
+      expect { User.create! }.to raise_error(ActiveRecord::RecordInvalid)
+    end
+  end
 end
