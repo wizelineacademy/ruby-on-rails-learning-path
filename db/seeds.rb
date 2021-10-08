890d1fa81785fd1d895daa6e7d1f8b63257aa04d
Gerardo Gonzalez <gerardo.gonzalez@wizeline.com>
2021-10-08 16:33:44 -0500

    Add service for connecting to api, added seeds for db

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+pokemons = [{name: "Example1"}, {name: "Example2"}, {name: "Example3"}]
+
+pokemons.each_with_index do |pokemon, i|
+	attrs = {
+    name: pokemon[:name]
+    base_hp: 10
+    base_exp: 10
+    weight: 10
+    height: 10
+    pokemon_api_id: 9999 + (i+1)
+  }
+
+  Pokemon.create(attrs)
+end
