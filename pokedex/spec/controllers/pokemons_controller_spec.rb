72f4353f214fea9f96377e95bd98da0749662b4b
Tania Reyes Martínez <tania.reyes@wizeline.com>
2021-11-12 19:42:10 -0600

    Tests fixed

--- pokedex/spec/controllers/pokemons_controller_spec.rb
+++ pokedex/spec/controllers/pokemons_controller_spec.rb
@@ -21,37 +21,45 @@ RSpec.describe PokemonsController, type: :controller do
 
   describe "GET show" do
     it "returns a 200" do
-      get :show, { id: @pokemon.id } 
+      get :show, params:{ id: @pokemon.id } 
       expect(response).to be_successful
     end
 
     it "renders 'show' template" do
-      get :show, { id: @pokemon.id } 
+      get :show, params:{ id: @pokemon.id } 
       expect(response).to render_template('show')
     end
   end
 
   describe "GET edit" do
     it "returns a 200" do
-      get :edit, { id: @pokemon.id } 
+      get :edit, params:{ id: @pokemon.id } 
       expect(response).to be_successful
     end
 
     it "renders 'edit' template" do
-      get :edit, { id: @pokemon.id } 
+      get :edit, params:{ id: @pokemon.id } 
       expect(response).to render_template('edit')
     end
   end
 
   describe "GET update" do
+    pokemon_params = {
+      :name => 'snorlax', 
+      :is_default => true, 
+      :order => 1, 
+      :weight => 1,  
+      :height => 1, 
+      :location_area_encounters => 'https://pokeapi.co/api/v2/pokemon/1/encounters'
+    }
     it "returns a 200" do
-      get :update, { id: @pokemon.id } 
+      get :update, params:{ id: @pokemon.id, pokemon: pokemon_params } 
       expect(response).to be_successful
     end
 
     it "renders 'update' template" do
-      get :update, { id: @pokemon.id } 
-      expect(response).to render_template('update')
+      get :update, params:{ id: @pokemon.id, pokemon: pokemon_params } 
+      expect(response).to render_template('edit')
     end
   end
 end
