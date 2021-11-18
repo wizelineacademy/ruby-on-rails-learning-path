cd8ed22418ca51e53661c9343798260906d6017a
jcaladwizeline <juan.calad@wizeline.com>
2021-11-18 15:48:50 -0500

    Activity 4 and some fixes

--- environment.rb
+++ environment.rb
@@ -2,27 +2,27 @@ module Environment
 
   class Depot
     attr_accessor :packs
-    
+
     def initialize
       @packs = {
-        simple_transportation_pack: {
-          intelligence => [:cellphone],
-          items => [:medipack, :chevy_versa]
+        :simple_transportation_pack=> {
+          :intelligence => [:cellphone],
+          :items => [:mediapack, :chevy_versa]
         },
-        standard_transportation_pack: {
-          intelligence => [:cellphone, :antenna],
-          arsenal => [:colt_1911],
-          items => [:handcuffs, :medipack, :chemistry, :chevy_versa]
+        :standard_transportation_pack=> {
+          :intelligence => [:cellphone, :antenna],
+          :arsenal => [:colt_1911],
+          :items => [:handcuffs, :medipack, :chemistry, :chevy_versa]
         },
-        simple_mission_pack: {
-          intelligence => [:infopack, :laptop, :cellphone, :antenna],
-          arsenal=> [:colt_1911],
-          items => [:handcuffs, :medipack, :chemistry, :financial]
+        :simple_mission_pack=> {
+          :intelligence => [:infopack, :laptop, :cellphone, :antenna],
+          :arsenal=> [:colt_1911],
+          :items => [:handcuffs, :medipack, :chemistry, :financial]
         },
-        standard_mission_pack: {
-          intelligence => [:infopack, :laptop, :cellphone, :antenna],
-          arsenal => [:remington_870, :colt_1911, :machete, :hatchet],
-          items => [:handcuffs, :medipack, :chemistry, :financial]
+        :standard_mission_pack=> {
+          :intelligence => [:infopack, :laptop, :cellphone, :antenna],
+          :arsenal => [:remington_870, :colt_1911, :machete, :hatchet],
+          :items => [:handcuffs, :medipack, :chemistry, :financial]
         }
       }
     end
@@ -33,23 +33,26 @@ module Environment
     attr_accessor :missions
 
     def new_mission(name, objective, pack)
+      @missions = {}
       @missions[name] = {
-        objective => objective,
-        status => :active,
-        pack => @depot.packs[pack]
+        :objective => objective,
+        :status => :active,
+        :pack => pack
       }
     end
 
-  end
+    %w(paused aborted failed accomplished).each do |state|
+      define_method("set_mission_to_#{state}") do |argument|
+        @missions[argument][:status] = state
+      end
+    end
 
-  %w(paused aborted failed accomplished).each do |state|
-    define_method("set_mission_to_#{state}") { |mission| @missions[mission][:status] = state }
   end
 
   class Human
     attr_accessor :id, :name, :personal_data, :professional_data
 
-    def initialize()
+    def initialize(name)
       @id = object_id
       @name = name
       @personal_data = {}
