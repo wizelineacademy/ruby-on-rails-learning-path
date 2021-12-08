fba274b9f0e4d1a82ff2ab660a09458c552bbfd2
Rafael Ruiz <rafael.ruiz.rodriguez@outlook.com>
2021-12-08 12:27:52 -0800

    +Se agregó redirección a root si la página no existe + Se agregó un poco de estilo en la sección de la información de los pokemon (falta pulir)

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,9 @@ Rails.application.routes.draw do
   resources :my_pokemons
   devise_for :trainers
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root 'my_pokemons#index'
-
+  root 'pokemons#index'
   resources :pokemons
   resources :my_pokemons  
   resources :trainers
+  get "*path" => redirect("/")
 end
