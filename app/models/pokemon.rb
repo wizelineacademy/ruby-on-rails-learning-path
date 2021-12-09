e91803f7d3429e994c91a7140fd18e76a96efcf4
Rodrigo Cervantes <rodrigo.delatorre@wizeline.com>
2021-12-09 14:13:42 -0600

    Adding Pokemon form validations.

--- app/models/pokemon.rb
+++ app/models/pokemon.rb
@@ -5,4 +5,9 @@ class Pokemon < ApplicationRecord
   has_many :stats, :through => :pokemons_stats
 
   scope :sorted, lambda { order("id ASC") }
+
+  validates :name, :presence => true
+  validates :height, :presence => true
+  validates :weight, :presence => true
+
 end
