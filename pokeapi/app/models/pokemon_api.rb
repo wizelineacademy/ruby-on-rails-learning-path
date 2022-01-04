70a17a0b2512bb2e6020c18e7625ce00024fcd1f
Yohana Tunarrosa <nelly.tunarrosa@wizeline.com>
2022-01-04 00:14:06 -0500

    validating data

--- pokeapi/app/models/pokemon_api.rb
+++ pokeapi/app/models/pokemon_api.rb
@@ -2,7 +2,7 @@ class PokemonApi
     def self.to_json
       pokemons = []
   
-        response = HTTParty.get("https://pokeapi.co/api/v2/pokemon?limit=20&offset=0")
+        response = HTTParty.get("https://pokeapi.co/api/v2/pokemon?limit=2&offset=0")
         response = JSON.parse(response.body)
         response.each do |k, value|
             if k == "results"
@@ -23,11 +23,19 @@ class PokemonApi
             puts poke['height']
             abilities = poke['abilities']
             stats = poke['stats']
+            pokemon = Pokemon.create(
+                name: poke['forms'][0]['name'],
+                base_experience: poke['base_experience'],
+                height: poke['height'],
+                weight: poke['weight'],
+                img: poke['sprites']['other']['dream_world']["front_default"]
+            )
             abilities.map do |ability|
                 puts ability
                 puts ability['ability']['name']
                 response = HTTParty.get(ability['ability']["url"])
                 puts response["id"]
+                poke=Pokemon.where(name: poke['forms'][0]['name'])
             end
             stats.map do |stat|
                 puts stat
